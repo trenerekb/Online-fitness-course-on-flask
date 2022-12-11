9dc04c9d4df1e4c913764e85ec9ae1f078ca47b6
Руслан <101623951+trenerekb@users.noreply.github.com>
2022-12-11 14:51:57 +0500

    Delete comments directory

--- comments/blueprint.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-
-comments = Blueprint('comments', __name__, template_folder='templates')
-
-
-@comments.route('/')
-def index():
-    return render_template('comments/index.html')
